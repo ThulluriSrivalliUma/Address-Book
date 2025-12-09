e73079dec4a76d6e7be81b1d2ef6ce4f4d9f762e
ThulluriSrivalliUma <thullurisrivalli@gmail.com>
2025-12-09 18:10:46 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Contact c1= new Contact("Abc","Xyz","AB123","Abcd","Telangana","1234567890","abc@123",11111);
+        System.out.println(c1);
+    }
+}
